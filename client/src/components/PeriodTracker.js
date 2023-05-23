274518a027c4f38e51b9fd4de212ecd7faa053c2
Dhruv Bhatia <dbhatia03@gmail.com>
2023-05-23 15:41:48 +0530

    first commit

---
+++ client/src/components/PeriodTracker.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+
+const PeriodTracker = () => {
+  const [startDate, setStartDate] = useState("");
+  const [cycles, setCycles] = useState([]);
+
+  const handleStartDateChange = (e) => {
+    setStartDate(e.target.value);
+  };
+
+  const addCycle = (startDate) => {
+    const newCycle = {
+      startDate,
+      daysSincePreviousCycle: calculateDaysDifference(startDate),
+    };
+
+    setCycles([...cycles, newCycle]);
+    setStartDate("");
+  };
+
+  const calculateDaysDifference = (date) => {
+    if (cycles.length > 0) {
+      const lastCycle = cycles[cycles.length - 1];
+      const oneDay = 24 * 60 * 60 * 1000; // One day in milliseconds
+      const diffDays = Math.round((new Date(date) - new Date(lastCycle.startDate)) / oneDay);
+      return diffDays;
+    }
+    return "";
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (startDate !== "") {
+      addCycle(startDate);
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>Menstruation Tracker</h1>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="start-date">Cycle Start Date:</label>
+        <input
+          type="date"
+          id="start-date"
+          name="start-date"
+          value={startDate}
+          onChange={handleStartDateChange}
+        />
+
+        <input type="submit" value="Add Cycle" />
+      </form>
+
+      <table id="cycle-table">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Days Since Previous Cycle</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cycles.map((cycle, index) => (
+            <tr key={index}>
+              <td>{cycle.startDate}</td>
+              <td>{cycle.daysSincePreviousCycle}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default PeriodTracker;
